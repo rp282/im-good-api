bc98dc78d4407bbff8bfa5c54e7414e8e2ca2c53
Ryan <ryanpun282@gmail.com>
2023-02-27 23:24:15 -0800

    interactions 0.1

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import '../styles/globals.css'
+import {
+  TEST_COMMAND,
+  HasGuildCommands,
+} from '../commands';
 
 function MyApp({ Component, pageProps }) {
+  HasGuildCommands(process.env.APP_ID, process.env.GUILD_ID, [TEST_COMMAND])
   return <Component {...pageProps} />
 }
 
--- pages/api/interactions/index.js
+++ pages/api/interactions/index.js
@@ -1,7 +1,31 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
+import {
+    InteractionType,
+    InteractionResponseType,
+    InteractionResponseFlags,
+    MessageComponentTypes,
+    ButtonStyleTypes,
+  } from 'discord-interactions';
+  
 export default function handler(req, res) {
     console.log({req})
+    
+    const { type, id, data } = JSON.parse(req.body);
+    if (type === InteractionType.APPLICATION_COMMAND) {
+        const { name } = data;
+    
+        // "test" guild command
+        if (name === 'test') {
+          // Send a message into the channel where command was triggered from
+          return res.send({
+            type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+            data: {
+              // Fetches a random emoji to send from a helper function
+              content: `I'm good.`,
+            },
+          });
+        }
+      }
     res.status(200).json({ name: 'John Doe' })
   }
   
