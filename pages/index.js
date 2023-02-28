7b8d65547678159338a0b40c097b32dfaf79dd51
Ryan <ryanpun282@gmail.com>
2023-02-27 23:03:23 -0800

    interactions

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,15 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+
+  function handleSend() {
+    fetch('/api/')
+  }
+
   return (
     <div className={styles.container}>
       I'm good.
+      <button onClick={() => handleSend()}>Send Message</button>
     </div>
   )
 }
